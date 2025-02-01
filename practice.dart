6609314307e08b20794b3c66f400d1d5a9398d69
rshhridoy <rshhridoy@gmail.com>
2025-02-01 22:03:41 +0600

    practice

---
+++ practice.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+void main(){
+  print("Welcome to Dart Programming!!");
+
+/*   stdout.write("Enter your name: ");
+  var name = stdin.readLineSync();
+
+  print("Your name is: $name"); */
+
+
+  var sub = ['Math','CS','Flutter', 'Web'];
+
+  var info = {
+    "math": 90,
+    "CS": 40,
+    "Flutter": 50,
+    "Web": 33
+  };
+
+  print(sub.length);
+  print(info);
+  print(info["CS"]);
+}
+
+
+class Hridoy{
+
+  void name(){
+    print("Hridoy");
+  }
+
+  
+
+  
+}
